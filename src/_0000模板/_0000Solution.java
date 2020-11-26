6fcd544b027efdec1a9b49062836b99e14e6a4e3
user.name <user.name@example.com>
2020-11-26 13:23:03 +0800

    添加模板文件

---
+++ src/_0000模板/_0000Solution.java
@@ -0,0 +1,10 @@
+package _0000模板;
+
+import java.util.Scanner;
+
+public class _0000Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
